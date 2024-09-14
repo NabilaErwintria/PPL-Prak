a2ac913aa76eb2c2829075f632bc8857bddaaed4
bilionr <go.billy.alexander@gmail.com>
2024-09-14 22:39:26 +0700

    tombol pindah page

---
+++ resources/views/home/logbook.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitoring PKL</title>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v113_1089">
+        <div class="name"></div>
+        <span class="v113_1116">Laporan</span>
+        <div class="v123_1694">
+            <div class="v123_1695">
+                <div class="name"></div>
+                <div class="name"></div>
+                <span class="v123_1698">Abi</span>
+                <span class="v123_1699">187345678</span>
+            </div>
+        </div>
+        <div class="v113_1090">
+            <span class="v113_1091">Monitoring PKL</span>
+            <div class="v144_287">
+            <button onclick="location.href='{{ url('profil') }}'">
+                <div class="v113_1092">
+                    
+                    <span class="v113_1094">Profil</span>
+                    
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('download') }}'">
+                <div class="v113_1095">
+                    <span class="v113_1097">Download</span>
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('pengajuan') }}'">
+                <div class="v113_1098">
+                    <div class="name"></div>
+                    <span class="v113_1100">Pengajuan PKL</span>
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('informasi') }}'">
+                <div class="v113_1101">
+                    <div class="name"></div>
+                    <span class="v113_1103">Informasi PKL</span>
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('logbook') }}'">
+                <div class="v113_1104">
+                    <span class="v113_1106">Logbook</span>
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('bimbingan') }}'">
+                <div class="v113_1107">
+                    <span class="v113_1109">Bimbingan</span>
+                    <div class="name"></div>
+                </div>
+            </button>
+            <button onclick="location.href='{{ url('laporan') }}'">
+                <div class="v120_1310">
+                    <div class="name"></div>
+                    <span class="v120_1312">Laporan</span>
+                    <div class="name"></div>
+                </div>
+            </div>
+        </div>
+        <div class="v118_1244">
+            <div class="v113_1117">
+                <span class="v113_1118">Dokumen</span>
+            </div>
+            <div class="v113_1119">
+                <span class="v113_1120">Template Surat Pengantar PKL</span>
+            </div>
+            <div class="v113_1121">
+                <span class="v113_1122">Kartu Monitoring Bimbingan</span>
+            </div>
+            <div class="v113_1123">
+                <span class="v113_1124">Template Laporan PKL</span>
+            </div>
+            <div class="v113_1125">
+                <span class="v118_1243">Formulir Permintaan Dosen Bimbingan</span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/download', function () {
+    return view('home/download');
+});
+
+Route::get('/bimbingan', function () {
+    return view('home/bimbingan');
+});
+
+Route::get('/informasi', function () {
+    return view('home/informasi');
+});
+
+Route::get('/laporan', function () {
+    return view('home/laporan');
+});
+
+Route::get('/logbook', function () {
+    return view('home/logbook');
+});
+
+Route::get('/pengajuan', function () {
+    return view('home/pengajuan');
+});
+
+Route::get('/profil', function () {
+    return view('home/profil');
+});
+
+
+
+/*
+
+| Jangan lupa php artisan route:clear
+
+*/
